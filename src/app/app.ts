8d47ddf0bbb528a21a0057f26f32c5e8558e876a
Bob <dash78231@gmail.com>
2025-10-13 17:32:40 +0800

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,57 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeroSection } from './hero-section/hero-section';
+import { Feature } from './feature/feature';
+import { HotProducts } from './hot-products/hot-products';
+import { NewProducts } from './new-products/new-products';
+import { CountryRoad } from './country-road/country-road';
+import { HowlCastle } from './howl-castle/howl-castle';
+import { Totoro } from './totoro/totoro';
+import { Kiki } from './kiki/kiki';
+import { Information } from './information/information';
+import { ProductsPreview } from './products-preview/products-preview';
+import { Store } from './store/store';
+import { Crumb } from './crumb/crumb';
+import { Footer } from './footer/footer';
+import { HeaderNav } from './header-nav/header-nav';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    HeroSection,
+    Feature,
+    HotProducts,
+    NewProducts,
+    CountryRoad,
+    HowlCastle,
+    Totoro,
+    Kiki,
+    Information,
+    ProductsPreview,
+    Store,
+    Crumb,
+    Footer,
+    HeaderNav,
+  ],
+  template: `
+    <app-header-nav />
+    <app-hero-section />
+    <app-feature />
+    <app-hot-products />
+    <app-new-products />
+    <app-country-road />
+    <app-howl-castle />
+    <app-totoro />
+    <app-kiki />
+    <app-information />
+    <app-products-preview />
+    <app-store />
+    <app-crumb />
+    <app-footer />
+  `,
+  styles: ``,
+})
+export class App {
+  protected readonly title = signal('DonguriRepublic');
+}
